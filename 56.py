b5468448c603b819358fa036b950e17d451d5dde
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2020-07-17 19:26:35 +0530

    Add files via upload

---
+++ 56.py
@@ -0,0 +1,9 @@
+import os
+hw = os.get_terminal_size()
+print("Height:", hw[1], "Width:", hw[0])
+
+
+
+import os
+hw = os.get_terminal_size()
+print(hw[1], hw[0])
---
+++ 58.py
@@ -0,0 +1,5 @@
+def sum_of_number(n):
+    return n * (n + 1) / 2
+
+
+print(sum_of_number(9))
