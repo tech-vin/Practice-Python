3730d6d0135339675b418e68b8272f1c766c2958
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2020-07-19 22:07:33 +0530

    Add files via upload

---
+++ 73.py
@@ -0,0 +1,13 @@
+# mid-point of a line
+# between (4, 4) (2, 2) --> (3, 3) is in the center.
+
+x1 = float(input('Enter x1: '))
+y1 = float(input('Enter y1: '))
+x2 = float(input('Enter x2: '))
+y2 = float(input('Enter y2: '))
+
+x_m_point = (x1 + x2) / 2
+y_m_point = (y1 + y2) / 2
+
+print(x_m_point)
+print(y_m_point)
