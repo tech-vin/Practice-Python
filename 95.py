6df09d3399ea9b32d697e704d704d639fdecc544
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2020-07-21 19:33:34 +0530

    Add files via upload

---
+++ 95.py
@@ -0,0 +1,7 @@
+# check if string is numerics 
+
+str = '123'
+if str.isdigit():
+    print('True')
+else:
+    print('False')
---
+++ 96.py
@@ -0,0 +1,6 @@
+import traceback
+print()
+def f1():return abc()
+def abc():traceback.print_stack()
+f1()
+print()
