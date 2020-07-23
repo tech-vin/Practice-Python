866c6ec93fc8431e7168efe1e7e7128ba85f7d86
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2020-07-23 19:59:32 +0530

    Add files via upload

---
+++ 115.py
@@ -0,0 +1,5 @@
+my_list = [2, 5, 5, 8]
+
+product = list(filter(lambda x: x*x, my_list))
+
+print(product)
---
+++ 117.py
@@ -0,0 +1,5 @@
+str1 = 'vineet'
+str2 = 'vineet'
+
+print(hex(id(str1)))
+print(hex(id(str2)))
