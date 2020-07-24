97c84717e63e21037a2be1c9b5b95acf5d43e73e
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2020-07-24 20:48:20 +0530

    Add files via upload

---
+++ 129.py
@@ -0,0 +1,6 @@
+str1 = '507.22'
+str1 = str1.ljust(8, '0')
+print(str1)
+
+str2 = str1.rjust(10, '0')
+print(str2)
---
+++ 130.py
@@ -0,0 +1,4 @@
+import json
+
+print(json.dumps({'Vin': 100, 'Ayush': 20, 'Anil': 50}))
+
